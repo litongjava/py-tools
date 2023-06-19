3c877ecd5cb15d77fda648d712d916bd5905b368
litongjava <litongjava@qq.com>
2023-06-19 13:59:07 +0800

    修复字幕文件读取,支持多行

--- src/srt/get-text-from-srt.py
+++ src/srt/get-text-from-srt.py
@@ -1,17 +1,27 @@
 # 使用python语言编写一个程序,从srt字幕文件中提取出文字,只要文字信息,不要序号和时间信息
 import sys
+
+
 def getText(filepath):
     global text_list, text
-    with open(filepath, 'r',encoding='utf-8') as f:
+    with open(filepath, 'r', encoding='utf-8') as f:
         content = f.readlines()
+
     text_list = []
-    for i in range(0, len(content), 4):
+    i = 2;
+    length = len(content)
+    while i < length:
         # 剔除行首的序号和时间信息,所以直接取第三行即可
-        text = content[i + 2].strip()
-        text_list.append(text)
+        text = content[i].strip()
+        if (text.strip()):
+            text_list.append(text)
+            i += 1;
+        else:
+            i += 3;
 
     text = ' '.join(text_list)
     print(text)
+
 
 # filepath = '/Users/ping/Movies/4K Video Downloader/The Importance of Culture in Tourism.en.srt'
 # getText(filepath)
@@ -19,4 +29,3 @@ def getText(filepath):
 if __name__ == "__main__":
     if sys.argv[1] is not None:
         getText(sys.argv[1])
-
